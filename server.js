c4fc3b705c460d0c975542fc9181bb829687466c
Alex <alexandru.bologan@stud.acs.upb.ro>
2025-05-09 23:39:21 +0300

    Add home route

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
+app.get('/', (req, res) => {
+  res.send('<h1>ESP32 Upload Server</h1><p>Go to <a href="/gallery">Gallery</a> to view photos.</p>');
+});
+
 // Static folders
 app.use('/uploads', express.static('uploads'));
 
